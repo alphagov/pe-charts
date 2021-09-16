822e0de0f3864141620825081ad287a2a1966209
Dan Jones <dan.jones@lunarfish.co.uk>
2021-09-16 14:36:51 +0100

    Upgrade node version to latest

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('node-sass'));
 const concat = require('gulp-concat');
 
 gulp.task('copy_assets.static', function() {
